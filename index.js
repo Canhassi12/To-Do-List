864fca500c26089bb47303faff901c07fda6f01f
Canhassi <arthurcanhassi@gmail.com>
2022-04-20 17:19:58 -0300

    add DOM js

--- index.js
+++ index.js
@@ -6,8 +6,46 @@ icon1.addEventListener("click", () => {
     let input = document.createElement('input')
     input.setAttribute('type' , 'checkbox')
     li.appendChild(input)
-    li.appendChild(document.createTextNode('tarefa'))
+    li.appendChild(document.createTextNode('New task'))
     ul.appendChild(li)
 });
 
-console.log(icon1);
+const icon2 = document.getElementById("icon2");
+
+icon2.addEventListener("click", () => {
+    let ul = document.querySelector('.ul2')
+    let li = document.createElement('li')
+    let input = document.createElement('input')
+    input.setAttribute('type' , 'checkbox')
+    li.appendChild(input)
+    li.appendChild(document.createTextNode('New task'))
+    ul.appendChild(li)
+});
+
+const icon3 = document.getElementById('icon3')
+
+icon3.addEventListener('click', () => {
+    let ul = document.querySelector('.ul3')
+    let li = document.createElement('li')
+    let input = document.createElement('input')
+    input.setAttribute('type' , 'checkbox')
+
+    li.appendChild(input)
+    li.appendChild(document.createTextNode('New task'))
+    ul.appendChild(li)
+});
+
+const icon4 = document.getElementById('icon4')
+
+icon4.addEventListener('click', () => {
+    let ul = document.querySelector('.ul4')
+    let li = document.createElement('li')
+    let input = document.createElement('input')
+    input.setAttribute('type' , 'checkbox')
+
+    li.appendChild(input)
+    li.appendChild(document.createTextNode('New task'))
+    ul.appendChild(li)
+})
+
+
