46d940e1a21be476ca40886e16e1be58f9e65e65
Canhassi <arthurcanhassi@gmail.com>
2022-04-20 19:21:15 -0300

    update commands

--- index.js
+++ index.js
@@ -1,51 +1,9 @@
-const icon1 = document.getElementById("icon1");
-
-icon1.addEventListener("click", () => {
-    let ul = document.querySelector('.ul1')
-    let li = document.createElement('li')
+function createLine(listClass) {
+    let list = document.querySelector(listClass)
+    let line = document.createElement('li')
     let input = document.createElement('input')
     input.setAttribute('type' , 'checkbox')
-    li.appendChild(input)
-    li.appendChild(document.createTextNode('New task'))
-    ul.appendChild(li)
-});
-
-const icon2 = document.getElementById("icon2");
-
-icon2.addEventListener("click", () => {
-    let ul = document.querySelector('.ul2')
-    let li = document.createElement('li')
-    let input = document.createElement('input')
-    input.setAttribute('type' , 'checkbox')
-    li.appendChild(input)
-    li.appendChild(document.createTextNode('New task'))
-    ul.appendChild(li)
-});
-
-const icon3 = document.getElementById('icon3')
-
-icon3.addEventListener('click', () => {
-    let ul = document.querySelector('.ul3')
-    let li = document.createElement('li')
-    let input = document.createElement('input')
-    input.setAttribute('type' , 'checkbox')
-
-    li.appendChild(input)
-    li.appendChild(document.createTextNode('New task'))
-    ul.appendChild(li)
-});
-
-const icon4 = document.getElementById('icon4')
-
-icon4.addEventListener('click', () => {
-    let ul = document.querySelector('.ul4')
-    let li = document.createElement('li')
-    let input = document.createElement('input')
-    input.setAttribute('type' , 'checkbox')
-
-    li.appendChild(input)
-    li.appendChild(document.createTextNode('New task'))
-    ul.appendChild(li)
-})
-
-
+    line.appendChild(input)
+    line.appendChild(document.createTextNode(' New task'))
+    list.appendChild(line)
+}
