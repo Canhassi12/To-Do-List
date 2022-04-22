dc71226b8765f65b4db86834cfc550c5a56af669
Canhassi <arthurcanhassi@gmail.com>
2022-04-21 23:26:51 -0300

    optimized js and html

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 function createLine(listClass) {
     let list = document.querySelector(listClass)
-    let line = document.createElement('li')
-    let input = document.createElement('input')
-    input.setAttribute('type' , 'checkbox')
-    line.appendChild(input)
-    line.appendChild(document.createTextNode(' New task'))
+
+    let input = createInputElement();
+       
+    line = createLineElement(input);
+
     list.appendChild(line)
+}
+
+function createInputElement() {
+    let inputSection = document.createElement('div')
+    let input = createInput()
+    let label = createLabel()
+    
+    inputSection.appendChild(input)
+    inputSection.appendChild(label)
+
+    return inputSection
+}
+
+function createLineElement(input) {
+    let line = document.createElement('li')
+    line.appendChild(document.createTextNode('New task'))
+    return line.appendChild(input)
+}
+
+function createInput() {
+    let input = document.createElement('input')
+    input.setAttribute('type', 'checkbox')
+    return input
+}
+
+function createLabel() {
+    let label = document.createElement('label')
+    label = document.createTextNode('New Task')
+    return label
 }
